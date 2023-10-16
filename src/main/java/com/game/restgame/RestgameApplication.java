2d1d8a09c0abdf6e0776272407c5f7503aad3ad3
unknown <joonasroska123@gmail.com>
2023-10-16 22:27:15 +0300

    first commit

---
+++ src/main/java/com/game/restgame/RestgameApplication.java
@@ -0,0 +1,13 @@
+package com.game.restgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/restgame/RestgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.restgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
